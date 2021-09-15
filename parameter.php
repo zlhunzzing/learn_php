4e375b6bcd73c013717c70bad85f1d478ec078a7
Jihun Kim <zlhunzzing@gmail.com>
2021-09-15 20:11:22 +0900

    url parameter

---
+++ parameter.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+  <meta charset="utf-8">
+</head>
+<body>
+  안녕하세요. <?php echo $_GET['job'] ?> <?php echo $_GET['name'] ?>님
+  <?php  /*
+  url: http://127.0.0.1/learn_php/parameter.php?name=zlhunzzing&job=개발자
+   */  ?>
+</body>
+</html>
